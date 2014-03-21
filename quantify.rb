1b31156432292ca89b94fe1efa9069a56ad92be4
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2014-03-21 11:48:18 +0000

    Get album prices from iTunes

--- quantify.rb
+++ quantify.rb
@@ -2,6 +2,7 @@
 
 require_relative 'lib/spotifetch'
 require_relative 'lib/lastfetch'
+require_relative 'lib/itunesfetch'
 
 tracks = Spotifetch.fetch
 albums, individual_tracks = Spotifetch.group(tracks)
@@ -10,3 +11,5 @@ consistency_check = albums.inject(0){ |sum, album| sum + album['track_ids'].coun
 raise "Expected at least #{tracks.count} tracks but #{consistency_check} were found" unless consistency_check >= tracks.count
 
 enriched_albums = Lastfetch.fetch_albums(albums)
+
+itunes_albums = Itunesfetch.fetch_albums(enriched_albums)
