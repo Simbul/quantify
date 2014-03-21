f922ce17122f4c2735e36fa764a14ac42f738b26
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2014-03-21 11:26:30 +0000

    Rename main executable to `quantify`

--- quantify.rb
+++ quantify.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
 require_relative 'lib/spotifetch'
+require_relative 'lib/lastfetch'
 
 tracks = Spotifetch.fetch
 albums, individual_tracks = Spotifetch.group(tracks)
+
+enriched_albums = Lastfetch.fetch_albums(albums)
